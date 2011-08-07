a88222feba8a6917183e82fc06cd169747da5e84
Micah <micah@8thlight.com>
2011-08-06 22:20:24 -0500

    added mmargs to deps

--- joodo/project.clj
+++ joodo/project.clj
@@ -8,7 +8,8 @@
                  [ring/ring-core "0.3.8"]
                  [ring/ring-jetty-adapter "0.3.8"]
                  [compojure "0.6.2"]
-                 [hiccup "0.3.1"]]
+                 [hiccup "0.3.1"]
+                 [mmargs "1.2.0"]]
   :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/")
