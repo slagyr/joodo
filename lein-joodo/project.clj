1b4262a6c0769afe919ab63a4537d67995a3ca23
Dave Moore <dave@8thlight.com>
2012-02-21 23:22:19 -0600

    Hard-coded part of the classpath until we find a leiningen fix.

--- lein-joodo/project.clj
+++ lein-joodo/project.clj
@@ -1,4 +1,4 @@
-(defproject joodo/lein-joodo "0.7.1"
+(defproject joodo/lein-joodo "0.7.2"
   :description "Leiningen Plugin for Joodo, a Clojure framework for web apps."
   :license {:name "The MIT License"
             :url "file://LICENSE"
