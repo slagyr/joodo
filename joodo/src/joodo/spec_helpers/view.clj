37798b17be5ed3273cec12c97b0d1249cb760583
Myles Megyesi <mylesmegyesi@me.com>
2012-05-05 13:50:40 -0500

    fixes view spec helper

--- joodo/src/joodo/spec_helpers/view.clj
+++ joodo/src/joodo/spec_helpers/view.clj
@@ -3,7 +3,8 @@
   (:use
     [speclj.core :only (around)]
     [clojure.xml :only (parse)]
-    [joodo.views :only (*view-context* render-template render-hiccup render-html)])
+    [joodo.views :only (*view-context* render-template render-hiccup render-html)]
+    [joodo.core :only (->options)])
   (:import
     [speclj SpecFailure]))
 
