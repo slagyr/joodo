b780e8dd99792f67b0b96c876a8c33fcbf9dd98c
Micah <micah@8thlight.com>
2012-05-10 17:21:14 -0500

    added more casing function to chee.string

--- chee/spec/chee/string_spec.clj
+++ chee/spec/chee/string_spec.clj
@@ -13,13 +13,56 @@
     (should= "ABC XYZ" (gsub "CAB ZXY" #"(\w)(\w)(\w)" (fn [m] (str (nth m 2) (nth m 3) (nth m 1)))))
     (should= "1..2..3" (gsub "1.2.3" #"\." (fn [_] ".."))))
 
-  (it "uses dashes in spear-case"
+  (it "splits camel humps"
+    (should= "Ab Cd" (separate-camel-humps "AbCd"))
+    (should= "Abc0 Xyz1" (separate-camel-humps "Abc0Xyz1")))
+  
+  (it "converts strings into titles"
+    (should= "Class Name" (title-case "Class Name"))
+    (should= "Class Name" (title-case "class_name"))
+    (should= "Once Upon A Time" (title-case "once_upon_a_time"))
+    (should= "Once Upon A Time" (title-case "once-upon-a-time"))
+    (should= "Ab C E Fg Hi J" (title-case "AbC-eFg-hiJ"))
+    (should= "Ab C E Fg Hi J" (title-case "AbC_eFg_hiJ"))
+    (should= "With Spaces" (title-case "with spaces"))
+    (should= "Some Title" (title-case "Some Title"))
+    (should= "Some Title" (title-case "SomeTitle")))
+  
+  (it "can camalize strings"
+    (should= "defaultSceneName" (camel-case "defaultSceneName"))
+    (should= "setDefaultSceneName" (camel-case "set defaultSceneName"))
+    (should= "className" (camel-case "class_name"))
+    (should= "onceUponATime" (camel-case "once_upon_a_time"))
+    (should= "withSpaces" (camel-case "with spaces"))
+    (should= "withDash" (camel-case "with-dash"))
+    (should= "startingCapital" (camel-case "starting Capital")))
+  
+  (it "can do capital camel case too"
+    (should= "DefaultSceneName" (capital-camel-case "DefaultSceneName"))
+    (should= "DefaultSceneName" (capital-camel-case "defaultSceneName"))
+    (should= "ClassName" (capital-camel-case "class_name"))
+    (should= "OnceUponATime" (capital-camel-case "once_upon_a_time"))
+    (should= "WithSpaces" (capital-camel-case "with spaces"))
+    (should= "WithDash" (capital-camel-case "with-dash")))
+  
+  (it "can snake-case strings"
+    (should= "class_name" (snake-case "class_name"))
+    (should= "class_name" (snake-case "ClassName"))
+    (should= "one_two_three" (snake-case "OneTwoThree"))
+    (should= "one" (snake-case "One"))
+    (should= "one_two_three" (snake-case "one-two-three"))
+    (should= "one_two_three" (snake-case "one two three")))
+
+  (it "can spear-case strings"
     (should= "normal-name" (spear-case "NormalName"))
     (should= "one" (spear-case "One"))
     (should= "one-two" (spear-case "OneTwo"))
     (should= "one-two-three" (spear-case "OneTwo-Three"))
+    (should= "one-two-three" (spear-case "one_two-Three"))
     (should= "four" (spear-case "FOUR"))
     (should= "fou-r" (spear-case "FOuR"))
-    (should= "fi-ve" (spear-case "FI_VE"))))
+    (should= "fi-ve" (spear-case "FI_VE")))
+  
+  )
 
 (run-specs)
--- chee/src/chee/string.clj
+++ chee/src/chee/string.clj
@@ -17,8 +17,53 @@
           (.end matcher))
       (apply str (conj result (.substring value last-end))))))
 
-(defn spear-case [value]
-  (str/lower-case
-    (gsub
-      (str/replace (name value) "_" "-")
-      #"([a-z])([A-Z])" (fn [[_ lower upper]] (str lower "-" upper)))))
+(def hump-pattern #"[a-z0-9][A-Z]")
+(def non-camel-separator-pattern #"[_| |\-][A-Za-z]")
+(def non-snake-separator-pattern #"[ |\-]")
+(def non-spear-separator-pattern #"[ |\_]")
+
+(defn separate-camel-humps [value]
+  (gsub value hump-pattern
+    #(str/join " " (seq %))))
+
+(defn title-case
+  "Converts the input string, which may be in any form, to a title-case string
+  (title-case \"hello-world\") => \"Hello World\""
+  [value]
+  (str/join " "
+    (map str/capitalize
+      (str/split (separate-camel-humps value) #"[ |\-|_]"))))
+
+(defn camel-case
+  "Converts the input string, which may be in any form, to a camel-case string
+  (title-case \"hello-world\") => \"helloWorld\""
+  [value]
+  (gsub value non-camel-separator-pattern
+    #(str/upper-case (apply str (rest %)))))
+
+(defn capital-camel-case
+  "Converts the input string, which may be in any form, to a capitalized camel-case string
+  (title-case \"hello-world\") => \"HelloWorld\""
+  [value]
+  (let [camel (camel-case value)]
+   (str (str/upper-case (.substring camel 0 1))
+     (.substring camel 1 (.length camel)))))
+
+(defn snake-case
+  "Converts the input string, which may be in any form, to a snake-case string
+  (title-case \"hello-world\") => \"hello_world\""
+  [value]
+  (str/replace
+    (str/lower-case (separate-camel-humps value))
+    non-snake-separator-pattern
+    "_"))
+
+(defn spear-case
+  "Converts the input string, which may be in any form, to a spear-case string.
+  (spear-case \"Hello World\") => \"hello-world\""
+  [value]
+  (str/replace
+    (str/lower-case (separate-camel-humps value))
+    non-spear-separator-pattern
+    "-"))
+
