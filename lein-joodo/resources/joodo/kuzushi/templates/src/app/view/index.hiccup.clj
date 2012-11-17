39f4864973694291955ded7de6c4cd035dbae340
Micah <micahmartin@gmail.com>
2012-11-17 08:23:14 -0600

    ready for 1.0.0

--- lein-joodo/resources/joodo/kuzushi/templates/src/app/view/index.hiccup.clj
+++ lein-joodo/resources/joodo/kuzushi/templates/src/app/view/index.hiccup.clj
@@ -1,18 +1,14 @@
 [:div {:style "text-align: center;"}
  [:img {:src "/images/joodo.png"}]
- [:h1 "Welcomes You!"]]
+ [:h1 "Welcome To Joodo!"]]
 
 [:h3 "Intro"]
-[:p "Joodo is a clojure framework for building web applications. The project home is at "
- [:a {:href "https://github.com/slagyr/joodo"} "https://github.com/slagyr/joodo"] "."
- "It consists of 3 parts"
- [:ul
-  [:li [:b "Kuzushi"] "- which means 'breaking balance', is a Leiningen plugin that supplies all the Geashi commands."]
-  [:li [:b "Tsukuri"] "- which means 'entry', is a library containing all the development tools."]
-  [:li [:b "Kake"] "- which means 'execution', is the runtime library that contains all the main APIs."]]]
+[:p "Joodo is a Clojure library for building web applications. The project home is "
+ [:a {:href "https://github.com/slagyr/joodo"} "https://github.com/slagyr/joodo"] "."]
 
 [:h3 "License"]
 [:p "Copyright (C) 2011-2012 Micah Martin All Rights Reserved."]
 [:p "Distributed under the The MIT License."]
 
+[:h3 "Meta"]
 [:p "This app was generated for !-APP_NAME-!."]
