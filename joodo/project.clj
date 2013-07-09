1cba3b11945ad7cbc1fd920aeb3782652d29c798
Micah <micahmartin@gmail.com>
2013-07-09 13:35:36 -0500

    1.2.1

--- joodo/project.clj
+++ joodo/project.clj
@@ -1,4 +1,4 @@
-(defproject joodo "1.2.0"
+(defproject joodo "1.2.1"
   :description "Joodo, a Clojure framework for web apps."
   :license {:name "The MIT License"
             :url "file://LICENSE"
