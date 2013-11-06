08e0dd526d627363ce6c83fd4edeba622b2367e7
Micah <micahmartin@gmail.com>
2013-11-06 15:18:01 -0600

    update version 2.0.0

--- template/project.clj
+++ template/project.clj
@@ -1,4 +1,4 @@
-(defproject joodo/lein-template "0.1.0"
+(defproject joodo/lein-template "2.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
